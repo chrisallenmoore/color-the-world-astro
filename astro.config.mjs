a444b98c4492a9eb842bf937904e167b419ffd37
Chris Allen Moore <chris@chrisallenmoore.com>
2022-09-04 19:33:54 +0700

    whoops

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://chrisallenmoore.github.io",
-  base: "/color-the-world-astro",
+  base: "/",
   trailingSlash: "always",
   vite: {
     ssr: {
