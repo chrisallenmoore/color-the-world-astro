caf0d651fcb7e3e4d9b7723c711f6f37c4163dde
Chris Allen Moore <chris@chrisallenmoore.com>
2022-09-04 19:58:35 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://chrisallenmoore.github.io",
+  base: "/",
   trailingSlash: "always",
   vite: {
     ssr: {
